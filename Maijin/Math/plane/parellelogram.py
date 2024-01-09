c9f63f1afcc4d33e5bce18638cd67dd6cb0801a7
KaRU3-dev <121723731+KaRU3-dev@users.noreply.github.com>
2024-01-09 11:23:03 +0900

    Add calcuration.

--- Maijin/Math/plane/parellelogram.py
+++ Maijin/Math/plane/parellelogram.py
@@ -2,6 +2,44 @@
 平行四辺形
 """
 
+import math
+from Maijin.Math.plane import triangle
+
 def area(w: float=0, h: float=0):
     return w * h
 
+def cal_side_triangle_area(h: float=0, t: float=0):
+    """
+    Calculate side trianlge
+    h (float): height
+    t (float): theta
+    @return (float): area
+    """
+    # Calculate side triangle's bottom
+    w = h / math.tan(t)
+    # Calculate side triangle's area
+    a = triangle.area(w, h)
+    # Return
+    return a
+
+def cal_side_triangle_l(w: float=0, h: float=0):
+    """
+    Calculate side triangle
+    w (float): width
+    h (float): height
+    @return (float): long side
+    """
+    # Calculate side triangle's long side
+    l = triangle.cal_l(w, h)
+    return l
+
+def cal_side_triangle_hypotenuse(w: float=0, h: float=0):
+    """
+    Calculate side triangle
+    w (float): width
+    h (float): height
+    @return (float): hypotenuse
+    """
+    # Calculate side triangle's long side
+    l = triangle.sidereal_length(w, h)
+    return l
--- Maijin/Math/plane/trapezoid.py
+++ Maijin/Math/plane/trapezoid.py
@@ -1,3 +1,47 @@
 """
 台形
 """
+
+from cmath import sqrt, tan
+
+def trapezoid_area(bottom, upper, height):
+    """
+    台形の面積を求める
+    """
+    return (bottom + upper) * height / 2
+
+def trapezoid_sidereal_length(bottom, upper, left, right):
+    """
+    台形の周囲の長さを求める
+    """
+    return bottom + upper + left + right
+
+def trapezoid_height(bottom, upper, left, right):
+    """
+    台形の高さを求める
+    """
+    return (left ** 2 - right ** 2 + bottom ** 2 - upper ** 2) / (2 * (bottom - upper))
+
+def trapezoid_bottom(height, left, right):
+    """
+    台形の下底を求める
+    """
+    return (2 * height * (left + right) - (left ** 2 - right ** 2)) / (2 * (left + right))
+
+def trapezoid_upper(height, left, right):
+    """
+    台形の上底を求める
+    """
+    return (2 * height * (left + right) + (left ** 2 - right ** 2)) / (2 * (left + right))
+
+def trapezoid_left(bottom, upper, height):
+    """
+    台形の左斜辺を求める
+    """
+    return sqrt((bottom - upper) ** 2 + height ** 2)
+
+def trapezoid_right(bottom, upper, height):
+    """
+    台形の右斜辺を求める
+    """
+    return sqrt((bottom - upper) ** 2 + height ** 2)
